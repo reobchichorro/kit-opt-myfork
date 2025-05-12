4f5904b36897fb42bf6ceced6d712c1003bd57f4
Rodrigo <reobcnegf@gmail.com>
2025-05-12 18:43:44 -0300

    Fixing subseq bug

--- MYMLP/Subsequence.cpp
+++ MYMLP/Subsequence.cpp
@@ -7,8 +7,8 @@ Subsequence::Subsequence() {
 // temp is distance[sigma_1.last][sigma_2.first];
 void Subsequence::Concatenate(Subsequence &sigma_1, Subsequence &sigma_2, double temp) {
     W = sigma_1.W + sigma_2.W;
-    T = sigma_1.T + temp + sigma_2.T;
     C = sigma_1.C + sigma_2.W * (sigma_1.T + temp) + sigma_2.C;
+    T = sigma_1.T + temp + sigma_2.T;
     first = sigma_1.first;
     last = sigma_2.last;
 }
